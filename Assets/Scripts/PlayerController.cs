852c59db133f874fd8e22376700565a8b00af3a6
Skulls23 <florian23russie@hotmail.fr>
2021-10-19 15:16:25 +0200

    Added the possibility to walk and run with ctrl

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,28 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+     
     }
 
     // Cyclic call
-    void FixedUpdate()
+    void Update()
     {
-        /////////////////
-        //  MOVEMENTS  //
-        /////////////////
 
-        if (Input.GetButton("Horizontal"))
-            transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, 0);
-
-        if (Input.GetButton("Vertical"))
-            transform.Translate(0, 0, Input.GetAxis("Vertical") * speed * Time.deltaTime);
-
-        if (Input.GetButton("Jump"))
-            transform.Translate(0, Input.GetAxis("Jump") * speed * Time.deltaTime, 0);
     }
 }
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public float moveSpeed = 5;
+    public float rotationSpeed = 0.5f;
+
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = transform.GetComponent<Animator>();
+    }
+
+    // Cyclic call
+    void Update()
+    {
+        ///////////////
+        //  FORWARD  //
+        ///////////////
+
+        //RUN
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.W))
+        {
+            animator.SetInteger("forward", 2);
+            transform.Translate(0, 0, Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime);
+        }
+
+        //WALK
+        if (Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.LeftShift))
+        {
+            animator.SetInteger("forward", 1);
+            transform.Translate(0, 0, Input.GetAxis("Vertical") * (moveSpeed/2) * Time.deltaTime);
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            animator.SetInteger("forward", 0);
+        }
+
+
+        ////////////
+        //  TURN  //
+        ////////////
+
+
+        if (Input.GetKey(KeyCode.A))
+        {
+
+            animator.SetInteger("turn", 1);
+            transform.Rotate(0.0f, Input.GetAxis("Horizontal") * rotationSpeed, 0.0f);
+        }
+
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            animator.SetInteger("turn", 2);
+            transform.Rotate(0.0f, Input.GetAxis("Horizontal") * rotationSpeed, 0.0f);
+        }
+
+        if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.A))
+        {
+            animator.SetInteger("turn", 0);
+        }
+        
+
+        ////////////
+        //  JUMP  //
+        ////////////
+
+
+        if (Input.GetButton("Jump"))
+        {
+            transform.Translate(0, Input.GetAxis("Jump") * moveSpeed * Time.deltaTime, 0);
+        }
+
+    }
+}
